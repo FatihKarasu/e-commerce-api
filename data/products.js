8ced18a3bda3540ce5726db26d8a31b4c4f49fdf
Fatih Karasu <78952713+FatihKarasu@users.noreply.github.com>
2022-05-21 14:17:44 +0300

    Added new endpoints

---
+++ data/products.js
@@ -0,0 +1,43 @@
+var products = [
+    {
+      id: "red-dress",
+      categoryId:"women-sub1",
+      title: "Red Dress",
+      image: "images/product1.jpg",
+      description: "Happiness dress",
+      price: "279.99",
+      old_price: "319.99",
+    },
+    {
+      id: "yellow-dress",
+      categoryId:"women-sub1",
+      title: "Yellow Dress",
+      image: "images/product2.jpg",
+      description: "Indian kashmiri kaftaan",
+      price: "229.99",
+      old_price: "249.99",
+    },
+    {
+      id: "green-dress",
+      categoryId:"women-sub1",
+      title: "Green Dress",
+      image: "images/product3.jpg",
+      description: "A beautiful long green dress on a hanger on a white wall",
+      price: "219.99",
+      old_price: "249.99",
+    },
+    {
+      id: "red-dress-2",
+      categoryId:"women-sub1",
+      title: "Red Dress 2",
+      image: "images/product4.jpg",
+      description: "A beautiful red dress",
+      price: "189.99",
+      old_price: "199.99",
+    },
+  ];
+
+
+  module.exports = {
+    products,
+  };
--- routes/products.js
+++ routes/products.js
@@ -1,57 +1,29 @@
 const express = require("express");
+const{products}=require("../data/products")
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json(products);
 });
-router.post("/", (req, res) => {
-  res.send("projects");
-});
-router.get("/:projectId", (req, res) => {
-  let projectId = req.params.projectId;
 
-  res.send(projectId);
+router.get("/:productId", (req, res) => {
+  let productId= req.params.productId;
+  let result = products.filter((p)=>(p.id===productId))
+
+  res.json(result);
+});
+router.get("/search/:query", (req, res) => {
+  let query = req.params.query;
+  let result = products.filter(
+    (p, index) =>
+      (p.title.toLowerCase().includes(query.toLowerCase()) ||
+        p.description.toLowerCase().includes(query.toLowerCase())) &&
+      index < 5
+  );
+
+  res.json(result);
 });
 
 module.exports = router;
 
-let products = [
-  {
-    id: 1,
-    title: "Red Dress",
-    image: "images/product1.jpg",
-    description:
-      "Happiness dress",
-      price: "279.99",
-      old_price: "319.99",
-  },
-  {
-    id: 2,
-    title: "Yellow Dress",
-    image: "images/product2.jpg",
-    description:
-      "Indian kashmiri kaftaan",
-    price: "229.99",
-    old_price: "249.99",
 
-  },
-  {
-    id: 3,
-    title: "Green Dress",
-    image: "images/product3.jpg",
-    description:
-      "A beautiful long green dress on a hanger on a white wall",
-    price: "219.99",
-    old_price: "249.99"
-
-  },
-  {
-    id: 4,
-    title: "Red Dress 2",
-    image: "images/product4.jpg",
-    description:
-      "A beautiful red dress",
-    price: "189.99",
-    old_price: "199.99"
-  },
-];
